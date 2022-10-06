6b49a6610943ef354b90d72106267dbbe4eb3ce0
chinonso22 <105933442+chinonso22@users.noreply.github.com>
2022-10-06 17:31:10 +0100

    Add files via upload

---
+++ reactwebsiteas-master/genesisfinal.jsx
@@ -0,0 +1,245 @@
+import React from "react"
+import { Link } from "gatsby"
+import { se2 } from './isslcontact.module.css'
+import { sectcontnt } from './isslcontact.module.css'
+import { herosectiontext } from './isslcontact.module.css'
+import { bodycontainer } from './isslcontact.module.css'
+import { btmsd } from './isslcontact.module.css'
+import { isslbodyofwork } from './isslcontact.module.css'
+import { nomediaquerygrid } from './isslcontact.module.css'
+import { autogrid } from './isslcontact.module.css'
+import { boxw } from './isslcontact.module.css'
+import { imside } from './isslcontact.module.css'
+import { txside } from './isslcontact.module.css'
+import { txside2} from './isslcontact.module.css'
+import { txside3 } from './isslcontact.module.css'
+import { rgtarw } from './isslcontact.module.css'
+import { nxtscr } from './isslcontact.module.css'
+import { splitsecond} from './isslcontact.module.css'
+import { topspanner } from './isslcontact.module.css'
+import { bottomspanner } from './isslcontact.module.css'
+import { imageitall} from './isslcontact.module.css'
+import { oursolution} from './isslcontact.module.css'
+import { esx } from './isslcontact.module.css'
+import { solutionsflex1 } from './isslcontact.module.css'
+import {strech1 } from './isslcontact.module.css'
+import { ico } from './isslcontact.module.css'
+import { hed } from './isslcontact.module.css'
+import { tx} from './isslcontact.module.css'
+import { strech2 } from './isslcontact.module.css'
+import { bpn} from './isslcontact.module.css'
+import { box1 } from './isslcontact.module.css'
+import { as } from './isslcontact.module.css'
+import { bs } from './isslcontact.module.css'
+import { ntnt } from './isslcontact.module.css'
+import { footersect } from './isslcontact.module.css'
+import { startconvo } from './isslcontact.module.css'
+import { fs } from './isslcontact.module.css'
+import { startconvotxt } from './isslcontact.module.css'
+import { startconvobtn } from './isslcontact.module.css'
+import { contactusbtn } from './isslcontact.module.css'
+import { footergrid } from './isslcontact.module.css'
+import { logos } from './isslcontact.module.css'
+import { logo } from './isslcontact.module.css'
+import { addresssd } from './isslcontact.module.css'
+import { addre } from './isslcontact.module.css'
+import { tele } from './isslcontact.module.css'
+import { mail } from './isslcontact.module.css'
+import { linksd } from './isslcontact.module.css'
+import { qkl } from './isslcontact.module.css'
+import { othlink } from './isslcontact.module.css'
+import { inputsd } from './isslcontact.module.css'
+import { getupdat } from './isslcontact.module.css'
+import { ourproducts } from './isslcontact.module.css'
+import { howitall } from './isslcontact.module.css'
+import { tpsd } from './isslcontact.module.css'
+import { hersec } from './isslcontact.module.css'
+import { inptbx } from './isslcontact.module.css'
+import { subbtn } from './isslcontact.module.css'
+
+import { paragraph1 } from './isslcontact.module.css'
+import { hersec3 } from './isslcontact.module.css'
+import { emptys } from './isslcontact.module.css'
+import { auto_grid } from './isslcontact.module.css'
+import { auto_grid2 } from './isslcontact.module.css'
+import { boxgrid } from './isslcontact.module.css'
+import { heas } from './isslcontact.module.css'
+import { bod } from './isslcontact.module.css'
+import { vision } from './isslcontact.module.css'
+import { sss } from './isslcontact.module.css'
+import { dddd } from './isslcontact.module.css'
+import { sstx } from './isslcontact.module.css'
+import { ssbod } from './isslcontact.module.css'
+import { sideofimgs } from './isslcontact.module.css'
+import {sideofimg} from './isslcontact.module.css'
+
+import { rvs } from './isslcontact.module.css'
+import {sideoftext} from './isslcontact.module.css'
+
+import { twosidecontent } from './isslcontact.module.css'
+
+import { emp } from './isslcontact.module.css'
+import { twgrid } from './isslcontact.module.css'
+import { twgridbod } from './isslcontact.module.css'
+import { meed } from './isslcontact.module.css'
+import { bttm } from './isslcontact.module.css'
+
+import { bdy } from './isslcontact.module.css'
+import {hdn} from './isslcontact.module.css'
+
+import {hersec2} from './isslcontact.module.css'
+import {submitting} from './isslcontact.module.css'
+import {formcontent} from './isslcontact.module.css'
+import {sideoftext} from './isslcontact.module.css'
+import {textarea} from './isslcontact.module.css'
+import {label} from './isslcontact.module.css'
+import {formcontent} from './isslcontact.module.css'
+import {slimbod} from './isslcontact.module.css'
+import {sideoftext} from './isslcontact.module.css'
+import {slimbod} from './isslcontact.module.css'
+import {boldhd} from './isslcontact.module.css'
+import {hersec4} from './isslcontact.module.css'
+
+
+
+import {meimg} from './assets/img/Rectangle 126.png'
+import Isslnavbar from '../isslnavbar/index'
+
+import styles from './_styles'
+import Myfooter from '../myfooter/index'
+
+
+const Isslcontact = (props) => (
+
+<>
+<Isslnavbar />
+
+<>
+  <div className={hersec4}>
+    <div className={bodycontainer}>
+      <div className={tpsd}>
+        <h3>Let’s Start a Conversation</h3>
+      </div>
+    </div>
+  </div>
+  <div className={bodycontainer}>
+    <div className={emptys}>
+      <div className={twosidecontent}>
+        <div className={sideoftext}>
+          <div className={boldhd}>How can we assist you?</div>
+          <div className={slimbod}>
+            <span>ISSL is here to help you; </span>
+            Our experts are available to answer any questions you might have.
+            Leave us a message.
+          </div>
+          <div className={slimbod}>
+            <span>View the usage of our platform: </span>
+            Request a custom demo
+          </div>
+          <div className={slimbod}>
+            <span>View our Products </span>
+            Our product is your gateway to motivated employees, satisfied
+            customers and increased revenue.
+          </div>
+          <div className={slimbod}>
+            <div className={boldhd}>Visit Us</div>
+            Our product is your gateway to motivated employees, satisfied
+            customers and increased revenue.
+          </div>
+          <div className={slimbod}>
+            <div className={boldhd}>Email Us</div>
+            mails@isslng.com
+          </div>
+        </div>
+        <div className={sideoftext}>
+          <form id={form}>
+            <div className={formbody}>
+              <div className={formcontent}>
+                <label htmlFor="">First Name</label>
+                <input
+                  type="text"
+                  name="from_name"
+                  id="from_name"
+                  required="true"
+                />
+              </div>
+              <div className={formcontent}>
+                <label htmlFor="">Last Name</label>
+                <input
+                  type="text"
+                  name="last_name"
+                  id="last_name"
+                  required="true"
+                />
+              </div>
+              <div className={formcontent}>
+                <label htmlFor="">Company Name</label>
+                <input
+                  type="text"
+                  name="company_name"
+                  id="company_name"
+                  required="true"
+                />
+              </div>
+              <div className={formcontent}>
+                <label htmlFor="">Company Email</label>
+                <input
+                  type="email"
+                  name="company_email"
+                  id="company_email"
+                  required="true"
+                />
+              </div>
+              <div className={formcontent}>
+                <label htmlFor="">Job Title</label>
+                <input
+                  type="text"
+                  name="job_title"
+                  id="job_title"
+                  required="true"
+                />
+              </div>
+              <div className={formcontent}>
+                <label htmlFor="">Business Type</label>
+                <input
+                  type="text"
+                  name="business_type"
+                  id="business_type"
+                  required="true"
+                />
+              </div>
+              <div className={formcontent}>
+                <label htmlFor="">Message</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  cols={30}
+                  rows={10} />
+              </div>
+              <input
+                type="submit"
+                id="button"
+                defaultValue="Send Email"
+                className={submitting}
+              />
+              <div className={button-area}>
+                <span />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</>
+
+
+
+
+<Myfooter />
+  
+</>
+
+)
+
+export default Isslcontact
